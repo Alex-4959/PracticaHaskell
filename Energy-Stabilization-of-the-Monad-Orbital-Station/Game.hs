7764d2c1711456cfab9abf55e6003bb3805febfa
Saimbron <u1994959@campus.udg.edu>
2026-06-08 20:47:49 +0200

    Add files via upload

--- Energy-Stabilization-of-the-Monad-Orbital-Station/Game.hs
+++ Energy-Stabilization-of-the-Monad-Orbital-Station/Game.hs
@@ -4,24 +4,75 @@ import Control.Monad (unless)
 
 import Utils (todo, maybeHead)
 import Controller (Input(..), viToInput)
+import Parser (Instance(..), parseInstance) -- Importem la instancia del parser per poder llegir el fitxer i fer proves
 
+--IMPRTANT!!!!
+-- Aquest primer commit sobre aquest fitxer en concret, es simplement una base, no es segur de que sigui aixis, mentre funcioni, pots canviar coses
+-- Ara mateix el que tinc fet, entenc perfectament fins al primer mètode de instance show JocMontad
+-- A partir de aquest metode mes o menys veig el que vaig fent, estic copiant jo a ma el codi pero no acabo de pillar que es el que estic fent del tot
+-- Perque ho sapigues, el codi de Parser.hs si que el podria donar per fet, aquest si que es facil i s'enten be, si no entens m'ho dius
+
+type posicio = (int, int)
+type tauler = [string]
+  
+data Nucli = Nucli [posicio]
+  deriving show ( Eq,show)
 
 -- This of course need to be adapted to your needs
-data JocMonad = JocMonad Int
+data JocMonad = JocMonad
+{
+  alcadaNucli :: Int,
+  nColumnes :: Int,
+  nFiles :: Int,
+  tauler :: tauler,
+  inici :: posicio,
+  reactor :: posicio,
+  nucli :: Nucli
+} deriving (Eq)
+
 
 instance Show JocMonad where
-  show _ = "PSS... finish me!"
+  show joc = 
+    unlines [mostraFila r Fila | (r,Fila) <- zip [0..] (tauler joc)]
+    where
+      posNucli = localitzaNucli (nucli joc)
+
+      mostraFila :: Int -> String -> String
+      mostraFila r fila = [mostraCasella (r,c) valor | (c,valor) <- zip [0..] fila]
+      
+      mostraCasella :: posicio -> Char -> Char
+      mostraCasella p valor
+        | p 'elem' posNucli = 'N'
+        | otherwise = valor
 
 
--- Mode de joc
+-- Mode de joc, pot ser AI, QUE HO FACI LA MAQUINA, O QUE HO FACI L'USUARI, ES A DIR NOSALTRES
 data Mode = AI | Huma
-
-
---
 -- I give you a dummy initial state, modify it as you need
---
-estatInicial = todo
-
+estatInicial :: Instance -> JocMonad
+estatInicial (Instance h cols rows t) = 
+  JocMonad
+  {
+    alcadaNucli = h,
+    nColumnes = cols,
+    nFiles = rows,
+    tauler = t,
+    inici = posInicial,
+    reactor = posFinal,
+    nucli = Nucli [posInicial]
+  }
+  where
+    posInicial = trobaCaracter 'S' t -- Metodes per trobar on esta el caracter S i G que representen el que seria la posicio inicial des de on començem,
+    -- i la posicio final a la qual tenim com ha objectiu arribar al acabar l'execució
+    posFinal = trobaCaracter 'G' t
+  
+  trobaCaracter :: Char -> Tauler -> posicio
+  trobaCaracter ch t =
+    case [(r,c) | (r,fila) <- zip [0..] t,
+                  (c,valor) <- zip [0..] fila,
+                  valor == ch] of
+      (p:_) -> p
+      [] -> error ("No s'ha trobat el caracter " ++ [ch] ++ " al tauler.")
 
 -- This is where the magic happens, you need to finish this function to make the JocMonad work,
 -- but I give you a dummy implementation to start with
@@ -29,32 +80,36 @@ juga :: Mode -> JocMonad -> IO()
 juga AI   = todo
 juga Huma = loop
     where
-      -- I give you this example of JocMonad loop,
-      -- but you can change it as you need
       loop :: JocMonad -> IO()
-      loop joc@(JocMonad n) = unless (finished joc) $ do
-        putStrLn $ "You have " ++ show n ++ " moves left. Enter a direction (h/j/k/l): "
+      loop joc = unless (finished joc) $ do
+        putStrLn $ "Introdueix una direccio (h/j/k/l): "
         putStrLn $ show joc
+
         input <- maybeHead <$> getLine
+
         case input >>= viToInput of
           Nothing -> do
-            putStrLn "Invalid input, try again."
+            putStrLn "Entrada incorrecta, torna-ho a provar."
             loop joc
+
           Just direc -> do
-            putStrLn $ "You entered: " ++ show direc
-            loop $ JocMonad (n - 1)
+            putStrLn $ "Has entrat: " ++ show direc
+            loop joc
 
 
 -- PSS... this is just a dummy implementation for the example, remove it if you don't need it
 finished :: JocMonad -> Bool
-finished (JocMonad n)
-  | n == 0    = True
-  | otherwise = False
+finished = connectatALReactor
 
+localitzaNucli :: Nucli -> [posicio]
+localitzaNucli (nucli ps) = ps
 
-localitzaNucli      = todo
+connectatALReactor :: JocMonad -> Bool
+connectatAlReactor joc = 
+  estaDret (nucli joc) &&
+  localitzaNucli (nucli joc) == [reactor joc]
 
-connectatAlReactor  = todo
+-- HE ARRIBAT FINS AQUI!!!
 
 esPlataforma        = todo
 
--- Energy-Stabilization-of-the-Monad-Orbital-Station/Parser.hs
+++ Energy-Stabilization-of-the-Monad-Orbital-Station/Parser.hs
@@ -1,26 +1,53 @@
 module Parser (
-    CoreHeight,
-    NColumns,
-    NRows,
-    TableLines,
+    AlcadaNucli,
+    NColumnes,
+    NFiles,
+    TaulerLinies,
     Instance(..),
     parseInstance
 ) where
 
-import Utils (todo)
+-- Primera línia: alçada del nucli
+-- Segona línia: nombre de files
+-- Tercera línia: nombre de columnes
+-- Resta de línies: tauler
 
---
--- You can change the `Instance` definition if you thing it is necessary,
--- but the `parseInstance` function must be able to parse the input file into an `Instance` value.
--- You can also add any helper functions you need to implement `parseInstance`.
---
+--He redefinit els noms dels tipus de variables, perque aixi em queda mes clar que es cada cosa
+type AlcadaNucli = Int
+type NColumnes = Int
+type NFiles = Int
+type TaulerLinies = [String]
 
-type CoreHeight = Int
-type NColumns = Int
-type NRows = Int
-type TableLines = [String]
-
-data Instance = Instance CoreHeight NColumns NRows TableLines deriving Show
+data Instance = Instance AlcadaNucli NColumnes NFiles TaulerLinies
+    deriving Show -- Per poder mostrar la instancia fent les proves del codi
+    --En aquest cas he fet la prova i el resultat es correcte
 
 parseInstance :: String -> Instance
-parseInstance = todo
+parseInstance input =
+    case netejaLinies input of
+        (hLine:rowsLine:colsLine:tableLines) ->
+            let h    = read hLine    :: AlcadaNucli
+                rows = read rowsLine :: NFiles
+                cols = read colsLine :: NColumnes
+            in comprovaInstance h cols rows tableLines
+
+        _ ->
+            error "Format incorrecte: el fitxer ha de tenir l'alçada del nucli, el nombre de files, el nombre de columnes i les línies del tauler."
+
+netejaLinies :: String -> [String]
+netejaLinies =
+    filter (not . null) . map (filter (/= '\r')) . lines
+    -- Primer de tot dividim el text en linies, per poder llegir correctament, cada argument que ens han d'entrar
+    -- Despres eliminem els possibles caracters\r per si de cas apareixen
+    -- i per ultim eliminem les linies que siguin buides, per evitar problemes alhora de llegir
+
+comprovaInstance :: AlcadaNucli -> NColumnes -> NFiles -> TaulerLinies -> Instance
+comprovaInstance h cols rows tableLines
+--Comprovem que les dades siguin coherents
+    | length tableLines /= rows = --Comprovem que el nombre real de files del tauler coincideixi amb el especificat
+        error "El nombre de línies del tauler no coincideix amb el nombre de files especificat."
+
+    | any (\fila -> length fila /= cols) tableLines = --Comprovem que totes les files tinguin el nomre correcte de columnes
+        error "El nombre de columnes en alguna línia del tauler no coincideix amb el nombre de columnes especificat."
+    | otherwise =
+        Instance h cols rows tableLines
